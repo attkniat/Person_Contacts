5dc6d77df9c294d8cf4bc5d26b133423964795d7
Nathan Soares Oliveira <50963930+attkniat@users.noreply.github.com>
2022-10-07 19:21:34 -0300

    Creating the crud methods.

---
+++ APIPersonContacts/PersonContacts/Data/ContactsRepository.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PersonContacts.Data
+{
+    internal class ContactsRepository
+    {
+        internal async static Task<List<Contact>> GetContactsAsync()
+        {
+            using (var db = new AppDBContext())
+            {
+                return await db.Contacts.ToListAsync();
+            }
+        }
+
+        internal async static Task<Contact> GetContactByIdAsync(int contactId)
+        {
+            using (var db = new AppDBContext())
+            {
+                return await db.Contacts.FirstOrDefaultAsync(contact => contact.ContactId == contactId);
+            }
+        }
+
+        internal async static Task<bool> CreateContactAsync(Contact contactToCreate)
+        {
+            using (var db = new AppDBContext())
+            {
+                try
+                {
+                    await db.Contacts.AddAsync(contactToCreate);
+
+                    return await db.SaveChangesAsync() >= 1;
+                }
+                catch (Exception e)
+                {
+                    return false;
+                }
+            }
+        }
+        internal async static Task<bool> UpdateContactAsync(Contact contactToUpdate)
+        {
+            using (var db = new AppDBContext())
+            {
+                try
+                {
+                    db.Contacts.Update(contactToUpdate);
+
+                    return await db.SaveChangesAsync() >= 1;
+                }
+                catch (Exception e)
+                {
+                    return false;
+                }
+            }
+        }
+
+        internal async static Task<bool> DeletePostAsync(int contactId)
+        {
+            using (var db = new AppDBContext())
+            {
+                try
+                {
+                    Contact contactToDelete = await GetContactByIdAsync(contactId);
+
+                    db.Remove(contactToDelete);
+
+                    return await db.SaveChangesAsync() >= 1;
+                }
+                catch (Exception e)
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
