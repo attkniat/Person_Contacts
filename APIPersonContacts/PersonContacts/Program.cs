d0bb9e52bdb878f9cf8721d8383f2f7d74b241d2
Nathan Soares Oliveira <50963930+attkniat@users.noreply.github.com>
2022-10-07 19:42:17 -0300

    Configuring the Swagger and program class

--- APIPersonContacts/PersonContacts/Program.cs
+++ APIPersonContacts/PersonContacts/Program.cs
@@ -1,9 +1,27 @@
+using Microsoft.OpenApi.Models;
+using PersonContacts.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CORSPolicy",
+        builder =>
+        {
+            builder
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .WithOrigins("http://localhost:3000"/*, "https://calm-water-04859b403.azurestaticapps.net"*/);
+        });
+});
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(swaggerGenOptions =>
+{
+    swaggerGenOptions.SwaggerDoc("v1", new OpenApiInfo { Title = "ASP.NET Minimal API Contacts CRUD", Version = "v1" });
+});
+
 
 var app = builder.Build();
 
@@ -11,33 +29,19 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(swaggerUIOptions =>
+    {
+        swaggerUIOptions.DocumentTitle = "ASP.NET Minimal API Contacts CRUD";
+        swaggerUIOptions.SwaggerEndpoint("swagger/v1/swagger.json", "ASP.NET Minimal API Contacts CRUD");
+        swaggerUIOptions.RoutePrefix = string.Empty;
+    });
 }
 
+app.UseCors("CORSPolicy");
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.MapGet("/get-all-contacts", async () => await ContactsRepository.GetContactsAsync());
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+
 
 app.Run();
-
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
