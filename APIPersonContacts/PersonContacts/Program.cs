b6e7a686af17c74c927aa98c4e73b7997ed5ee9c
Nathan Soares Oliveira <50963930+attkniat@users.noreply.github.com>
2022-10-09 14:43:31 -0300

    Replace the old url to a valid react client url

--- APIPersonContacts/PersonContacts/Program.cs
+++ APIPersonContacts/PersonContacts/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddCors(options =>
             builder
             .AllowAnyMethod()
             .AllowAnyHeader()
-            .WithOrigins("http://localhost:3000"/*, "https://calm-water-04859b403.azurestaticapps.net"*/);
+            .WithOrigins("http://localhost:3000", "https://red-ground-01dd95f0f.2.azurestaticapps.net");
         });
 });
 
