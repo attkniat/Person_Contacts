c95235039e29c54d7847163952b45980d5c1a6c7
Nathan Soares Oliveira <50963930+attkniat@users.noreply.github.com>
2022-10-09 16:03:55 -0300

    Uptating the CORDS url policy

--- APIPersonContacts/PersonContacts/Program.cs
+++ APIPersonContacts/PersonContacts/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddCors(options =>
             builder
             .AllowAnyMethod()
             .AllowAnyHeader()
-            .WithOrigins("http://localhost:3000", "https://red-ground-01dd95f0f.2.azurestaticapps.net");
+            .WithOrigins("http://localhost:3000", "https://wonderful-smoke-0e728aa0f.2.azurestaticapps.net/");
         });
 });
 
